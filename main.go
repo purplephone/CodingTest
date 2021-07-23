0c45b76be54d530e50135eb94fea1131b91b76bf
Kim_min_woo <alsdnsla44@naver.com>
2021-07-23 22:50:33 +0900

    1967 time over  :(

--- main.go
+++ main.go
@@ -3,19 +3,19 @@ package main
 import (
 	"bufio"
 	"os"
+	"sort"
+	"strconv"
 )
 
 var (
 	sc      *bufio.Scanner
 	wr      *bufio.Writer
-	visited []bool
-	result  []int
 	treeMap map[int]*Node
 )
 
 type Node struct {
 	data  int
-	child *Node
+	child []int
 }
 
 func init() {
@@ -27,17 +27,55 @@ func init() {
 func main() {
 	defer wr.Flush()
 	N := scanInt()
+	treeMap = make(map[int]*Node, N+1)
+	treeMap[1] = &Node{}
 	for i := 1; i < N; i++ {
-		p, c, d := scanInt(), scanInt(), scanInt()
-		if _, empty := treeMap[p]; !empty {
-			treeMap[p] = &Node{}
+		p, c, w := scanInt(), scanInt(), scanInt()
+		if _, empty := treeMap[c]; !empty {
+			treeMap[c] = &Node{data: w}
 		}
-		treeMap[p].child = append()
+		treeMap[p].child = append(treeMap[p].child, c)
 	}
+	var max int
+	for i := 1; i < N; i++ {
+		result := dfs2(i)
+		if max < result {
+			max = result
+		}
+	}
+	wr.WriteString(strconv.Itoa(max))
 }
 
-func dfs(index int) {
-	visited[index] = true
+func dfs(index int) int {
+	if len(treeMap[index].child) == 0 {
+		return treeMap[index].data
+	}
+	var max int
+	for _, tmp := range treeMap[index].child {
+		re := dfs(tmp)
+		if re > max {
+			max = re
+		}
+	}
+	return max + treeMap[index].data
+}
+
+func dfs2(index int) int {
+	var tmp []int
+	if len(treeMap[index].child) == 0 {
+		return 0
+	}
+	for _, i := range treeMap[index].child {
+		tmp = append(tmp, dfs(i))
+	}
+	if len(tmp) > 1 {
+		sort.Slice(tmp, func(i, j int) bool {
+			return tmp[i] > tmp[j]
+		})
+		return tmp[0] + tmp[1]
+	} else {
+		return tmp[0]
+	}
 }
 
 func scanInt() (n int) {
